d7e40ca4a0141567eb271502ab362f875beb3009
harpagon210 <35413982+harpagon210@users.noreply.github.com>
2019-06-04 17:47:39 -0500

    Create Config.js

---
+++ scripts/Config.js
@@ -0,0 +1,44 @@
+Config = {
+	MAINTENANCE_MODE: false,
+	CHAIN_ID: 'ssc-mainnet1',
+	RPC_URL: 'https://testapi.steem-engine.com/arangodb/blockchain',
+	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
+	CONVERTER_API: 'https://converter-api.steem-engine.com/api',
+	SCOT_API: 'https://scot-api.steem-engine.com/',
+	NATIVE_TOKEN: 'ENG',
+	STEEMP_ACCOUNT: 'steem-peg',
+	DISABLED_TOKENS: ['BTC', 'STEEM', 'SBD', 'BCC', 'XAP'],
+	PEGGED_TOKEN: 'STEEMP',
+	PEGGED_TOKENS: [
+		{
+			name: 'Steem',
+			symbol: 'STEEM',
+			pegged_token_symbol: 'STEEMP'
+		}, 
+		{
+			name: 'Bitcoin',
+			symbol: 'BTC',
+			pegged_token_symbol: 'BTCP'
+		}, 
+		{
+			name: 'Litecoin',
+			symbol: 'LTC',
+			pegged_token_symbol: 'LTCP'
+		},
+		{
+			name: 'EOS',
+			symbol: 'EOS',
+			pegged_token_symbol: 'EOSP'
+		}, 
+		{
+			name: 'Bitcoin Cash',
+			symbol: 'BCH',
+			pegged_token_symbol: 'BCHP'
+		}, 
+		{
+			name: 'Dogecoin',
+			symbol: 'DOGE',
+			pegged_token_symbol: 'DOGEP'
+		}
+	]
+}
